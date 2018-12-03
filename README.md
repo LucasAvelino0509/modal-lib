4384df3d31bb49229d351a143670ce43333d6890
LucasAvelino0509 <lucas.avelino1203@gmail.com>
2018-12-02 23:28:10 -0200

    B 2.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # modal-lib
-É uma biblioteca para simplificação na criação de Modais para sua pagina WEB
+  É uma biblioteca para simplificação na criação de Modais para sua pagina WEB
+## Versões:
+* b 1.0:
+  * Primeira versão funcional com funcionalidades básicas
+* B 1.1:
+  * Simplificação da abertura de modais
+  * Templates pré-estabelecidos
+  * Função multi-template
+* B 2.0
+  * Aperfeiçoamento da funcionalidade multi-modal1
+  * Agora os modais abertos são armazenados em uma lista
+  * Os modais são fechados ao clicar fora do modal ou apertar ESC
--- modal-lib-1_0.js
+++ modal-lib-1_0.js
@@ -1,8 +1,13 @@
 var $md = {
-  modal : '<div class="bckgModal"><div class="modal"><++CONTENT++></div></div>',
+  modal : '<div class="bckgModal"><div style="width:100%;height:100%;position:absolute;top:0;left:0;"  onclick="$md.closeModal(\'target__\')"></div><div class="modal"><++CONTENT++></div></div>',
   contentMDL: [],
   templateMDL: [],
-  openModal : function(fieldNames, fieldValues, content = this.contentMDL[0], template = this.templateMDL[0], target = "#modalMaster"){
+  start: function(a,b){
+    this.contentMDL.push(a.html());
+    this.templateMDL.push(b.html());
+  },
+  activeModais: [],
+  openModal : function(fieldNames, fieldValues, content = this.contentMDL[0], template = this.templateMDL[0], target = "#1"){
     if (typeof content == "number"){content = this.contentMDL[content];}
     if (typeof template == "number"){template = this.templateMDL[template];}
     var modalHtml = $md.modal.replace("<++CONTENT++>",content)
@@ -11,13 +16,32 @@ var $md = {
       for (var i = 0; i < fieldNames.length; i++) {
         templatePreenchido = templatePreenchido.replace(new RegExp(fieldNames[i],"g"),fieldValues[i]);
       }
-      modalHtml = modalHtml.replace('<++template++>',templatePreenchido)
-      modalHtml = modalHtml.replace('<TARGET>',target)
+      modalHtml = modalHtml.replace(new RegExp('template__',"g"), templatePreenchido);
+      modalHtml = modalHtml.replace(new RegExp('target__',"g"), target);
     }
+    this.closeModal(target);
+    this.activeModais.push(target);
     $(target).html(modalHtml);
     return modalHtml;
   },
-  closeModal : function(target = "#modalMaster") {
-    $(target).html("");
+  closeModal : function(target = "#1") {
+    if (target == "last") {target = this.activeModais[this.activeModais.length-1]}
+    if($(target).html().length>0){
+      var found = false;
+      for (var i = 0; i < this.activeModais.length; i++) {
+        if(this.activeModais[i] == target){found = true;}
+        if (found) {
+          $(this.activeModais[i]).html("");
+          delete this.activeModais[i];
+        }
+      }
+      this.activeModais = this.activeModais.filter(a => a !=null);
+    }
+  }
+}
+
+window.onkeydown = function functionName(e) {
+  if (e.keyCode == 27) {
+    $md.closeModal("last");
   }
 }
